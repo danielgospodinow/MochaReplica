3bd7e0b1a6445ad6a5ba66dd1e6f6899cba27db5
danielgospodinow <danielgospodinow@gmail.com>
2018-07-30 10:09:05 +0300

    Handle test failures

--- index.js
+++ index.js
@@ -2,9 +2,14 @@
 
 const TESTS = [
     function() { console.log('1'); },
+    function() { throw new Error('fail'); },
     function() { console.log('2'); },
 ];
 
 for (const test of TESTS) {
-    test();
+    try {
+        test();
+    } catch (e) {
+
+    }
 }
