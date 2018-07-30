8d83dcecd085355b5008b854774e2a19ae1596ba
danielgospodinow <danielgospodinow@gmail.com>
2018-07-30 12:37:55 +0300

    Added async functions support

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 
 const assert = require('assert');
 
-/** TEST RUNNER */
+/** TEST RUNNER *////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-function testRunner(tests) {
+async function testRunner(tests) {
     const testResults = [];
 
     for (const test of tests) {
         try {
-            test();
+            await test();
             testResults.push([test.name, null]);
         } catch (e) {
             testResults.push([test.name, e]);
@@ -17,60 +17,60 @@ function testRunner(tests) {
     }
 
     const result = {};
-    for (const [testName, testError] of  testResults) {
+    for (const [testName, testError] of testResults) {
         result[testName] = testError;
     }
 
     return result;
 }
 
-/** TEST REPORTER */
+/** TEST REPORTER */////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 function testReporter(testResults) {
     return Object.entries(testResults).map(([testName, testError]) => {
         if (testError) {
             return `${testName} - ${testError.stack}`;
         }
-        
+
         return `${testName} - OK`;
     }).join('\n');
 }
 
-/** FRAMEWORK TESTS */
+/** FRAMEWORK TESTS *///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 const frameworkTests = [
-    function runsTestsSequientially() {
+    async function runsTestsSequientially() {
         const marks = [];
         const tests = [
             function add1() { marks.push(1); },
             function add2() { marks.push(2); },
         ];
 
-        testRunner(tests);
+        await testRunner(tests);
 
         assert.deepEqual(marks, [1, 2]);
     },
 
-    function isFaultTolerant() {
+    async function isFaultTolerant() {
         const tests = [
             function fail() { throw new Error('fail'); },
         ]
 
         try {
-            testRunner(tests);
+            await testRunner(tests);
         } catch (e) {
             throw new Error('fail');
         }
     },
 
-    function testRunnerReturnsObjectDescribingTheSuite() {
+    async function testRunnerReturnsObjectDescribingTheSuite() {
         const failError = new Error('fail');
         const tests = [
-            function ok() {},
+            function ok() { },
             function fail() { throw failError; },
         ];
 
-        const testResults = testRunner(tests);
+        const testResults = await testRunner(tests);
 
         assert.deepEqual(testResults, {
             ok: null,
@@ -78,17 +78,47 @@ const frameworkTests = [
         });
     },
 
-    function reportsResultsWithStack() {
+    async function reportsResultsWithStack() {
         const tests = [
-            function ok() {},
+            function ok() { },
             function fail() { throw new Error('fail'); },
         ];
 
-        const testResults = testReporter(testRunner(tests));
+        let res = await testRunner(tests);
+        const testResults = testReporter(res);
 
         assert.ok(/ok - OK\nfail - Error: fail\n.*at/m.test(testResults));
     },
+
+    async function handlesAsyncFuncs() {
+        const tests = [
+            async function okTest() {
+                return new Promise(function (resolve, reject) {
+                    setTimeout(resolve, 1000, 'Blank');
+                });
+            },
+            async function okTest2() {
+                return new Promise(function (resolve, reject) {
+                    setTimeout(resolve, 3000, 'Blank');
+                });
+            },
+            async function failTest() {
+                return new Promise(function (resolve, reject) {
+                    setTimeout(reject, 1000, 'This is an error message');
+                });
+            },
+        ]
+
+        let testResults = await testRunner(tests);
+
+        assert.deepEqual(testResults, {
+            okTest: null,
+            okTest2: null,
+            failTest: 'This is an error message',
+        });
+    }
 ];
 
-
-console.log(testReporter(testRunner(frameworkTests)));
+testRunner(frameworkTests).then((res) => {
+    console.log(testReporter(res));
+}).catch(err => console.log(err));
